1011f808fe30f980be99c59cf22ff8df49f111c3
ODIA221 <senbusinessfk@gmail.com>
2022-10-28 17:45:59 +0200

    fin front

---
+++ views/test.js
@@ -0,0 +1,46 @@
+document.getElementById("inscription").addEventListener("submit", function(e){
+
+
+    var errors ;
+    var inputs = document.getElementsByTagName("input");
+    var email = document.getElementById("mailInscript");  
+
+
+    for (let i = 0; i< inputs.length -1; i++) {
+        if (!inputs[i].value.trim()) {
+            errors = "les champs avec astérix sont obligatoire !";
+            
+        }
+    }
+
+    if (email.value.indexOf("@", 0) < 0)                 
+    { 
+        e.preventDefault();
+        document.getElementById("mailValide").innerHTML = "Le mail est inavlide !";
+        email.focus(); 
+        return false; 
+    }    
+    if (email.value.indexOf(".", 0) < 0)                 
+    { 
+        e.preventDefault();
+        document.getElementById("mailValide").innerHTML = "Le mail est inavlide !"; 
+        email.focus(); 
+        return false; 
+    }  
+    if (errors) {
+        e.preventDefault();
+        document.getElementById("errors").innerHTML = errors;
+        return false;
+
+    }else{
+        document.getElementById("success").innerHTML = "connexion Réussie !";
+
+    }
+
+    return true; 
+    
+
+
+});
+
+
